29069794379e00661840d1c2db4d0a388d4d5664
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-08-23 15:33:28 -0300

    Performance update

--- travel-app/src/pages/Home/index.tsx
+++ travel-app/src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useTransition } from "react"
 import { useNavigate } from "react-router-dom";
-import { FaRegPaperPlane } from 'react-icons/fa'
 import { AiFillStar, AiFillAlert } from 'react-icons/ai'
 import api from "../../services/api";
 import Skeleton from 'react-loading-skeleton'
@@ -11,40 +10,35 @@ export default function Home() {
 
     const navigate = useNavigate();
     const [trips, setTrips] = useState<any[]>([]);
-    const [isPending, startTransition] = useTransition();
 
     useEffect(() => {
-        startTransition(() => {
-            async function loadApi() {
-                const response = await api.get('trips')
-                setTrips(response.data);
-            }
-            loadApi();
-        })
-    }, []);
+        async function loadApi() {
+            const response = await api.get('trips');
+            setTrips(response.data);
+        }
+        loadApi();
+    }, [setTrips]);
 
     async function details(id: any) {
         navigate(`/trips/${id}`)
     }
 
+
     return (
         <div className="container_home">
             <h1>Viagens</h1>
-            {isPending ? (<div className="pending"> <FaRegPaperPlane size={70} color="#3D8833" /> </div>) : (
-                <div className="box">
-                    {trips.map(trip => (
-                        <li key={trip.id}>
-                            <img src={trip.img} alt={trip.title} className='image' />
-                            <strong>{trip.title || <Skeleton count={1} />}</strong>
-                            <strong><AiFillStar />{trip.note}</strong>
-                            <span>{trip.status ? <AiFillAlert color="green" /> : <AiFillAlert color="red" />} {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
-                            <span>R$ {trip.price},00 p/ noite.</span>
-                            <button disabled={trip.status === 0} onClick={() => details(trip.id)}>{trip.status ? 'Mais Detalhes' : 'Indisponivel'}</button>
-                        </li>
-                    ))}
-                </div>
-            )}
-
+            <div className="box">
+                {trips.map(trip => (
+                    <li key={trip.id}>
+                        <img src={trip.img  || <Skeleton/>} alt={trip.title} className='image' />
+                        <strong>{trip.title || <Skeleton count={1} />}</strong>
+                        <strong><AiFillStar />{trip.note  || <Skeleton count={1} />}</strong>
+                        <span>{trip.status ? <AiFillAlert color="green" /> : <AiFillAlert color="red" />} {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+                        <span>R$ {trip.price},00 p/ noite.</span>
+                        <button disabled={trip.status === 0} onClick={() => details(trip.id)}>{trip.status ? 'Mais Detalhes' : 'Indisponivel'}</button>
+                    </li>
+                ))}
+            </div>
         </div>
     )
 }
