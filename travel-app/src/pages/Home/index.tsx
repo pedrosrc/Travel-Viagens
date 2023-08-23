b1e594c1071f920d29574e131b1c25cfbc9e64f6
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-08-22 22:26:30 -0300

    update in stylezation

--- travel-app/src/pages/Home/index.tsx
+++ travel-app/src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect, useTransition } from "react"
 import { useNavigate } from "react-router-dom";
-import {FaRegPaperPlane} from 'react-icons/fa'
+import { FaRegPaperPlane } from 'react-icons/fa'
 import { AiFillStar, AiFillAlert } from 'react-icons/ai'
 import api from "../../services/api";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import './home.css'
 
 export default function Home() {
@@ -19,7 +21,7 @@ export default function Home() {
             }
             loadApi();
         })
-    }, [startTransition]);
+    }, []);
 
     async function details(id: any) {
         navigate(`/trips/${id}`)
@@ -28,22 +30,21 @@ export default function Home() {
     return (
         <div className="container_home">
             <h1>Viagens</h1>
-            {isPending? (<div className="pending"> <FaRegPaperPlane size={50} color="#3D8833"/> </div>) : (
+            {isPending ? (<div className="pending"> <FaRegPaperPlane size={70} color="#3D8833" /> </div>) : (
                 <div className="box">
-                {trips.map(trip => (
-                    <li key={trip.id}>
-                        <img src={trip.img} alt={trip.title} className='image' />
-                        <strong>{trip.title}</strong>
-                        <strong><AiFillStar />{trip.note}</strong>
-                        <span>{trip.status ? <AiFillAlert color="green" /> : <AiFillAlert color="red" />} {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
-
-                        <span>R$ {trip.price},00 p/ noite.</span>
-                        <button disabled={trip.status === 0} onClick={() => details(trip.id)}>{trip.status ? 'Mais Detalhes' : 'Indisponivel'}</button>
-                    </li>
-                ))}
-            </div>
+                    {trips.map(trip => (
+                        <li key={trip.id}>
+                            <img src={trip.img} alt={trip.title} className='image' />
+                            <strong>{trip.title || <Skeleton count={1} />}</strong>
+                            <strong><AiFillStar />{trip.note}</strong>
+                            <span>{trip.status ? <AiFillAlert color="green" /> : <AiFillAlert color="red" />} {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
+                            <span>R$ {trip.price},00 p/ noite.</span>
+                            <button disabled={trip.status === 0} onClick={() => details(trip.id)}>{trip.status ? 'Mais Detalhes' : 'Indisponivel'}</button>
+                        </li>
+                    ))}
+                </div>
             )}
-            
+
         </div>
     )
 }
--- travel-app/src/pages/Trip/index.tsx
+++ travel-app/src/pages/Trip/index.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { useParams } from "react-router-dom"
 import { useDispatch } from "react-redux";
 import { addReserveRequest } from "../../store/modules/reserves/actions";
 import api from "../../services/api"
 import { AiFillStar } from 'react-icons/ai'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import './trip.css'
 
 export default function Trip() {
@@ -23,9 +25,8 @@ export default function Trip() {
                 })
         }
         loadTrip();
-
     }, []);
-    
+
 
     async function handleAdd(id: any) {
         dispatch(addReserveRequest(id));
@@ -33,14 +34,14 @@ export default function Trip() {
 
     return (
         <div className="container_tr">
-            <img src={trip.img} alt="foto de praia" loading="lazy" />
+            <img src={trip.img || <Skeleton height={200} />} alt="foto de praia" loading="lazy" />
             <div className="box_info">
-                <h1>{trip.title}</h1>
+                <h1>{trip.title || <Skeleton count={1} />}</h1>
                 <div className="details">
-                    <h3>R$ {trip.price},00</h3>
-                    <h3><AiFillStar color="#ecd500" />{trip.note}</h3>
+                    <h3>R$ {trip.price || <Skeleton count={1} />},00</h3>
+                    <h3><AiFillStar color="#ecd500" />{trip.note || <Skeleton count={1} />}</h3>
                 </div>
-                <p>{trip.description}</p>
+                <p>{trip.description || <Skeleton count={6} />}</p>
                 <button onClick={() => handleAdd(trip.id)}>Reservar</button>
             </div>
         </div >
